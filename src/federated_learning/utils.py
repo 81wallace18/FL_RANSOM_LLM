e380377ab515ff071357760bbf3b8e8c2c5a79f6
81wallace18 <patrickwallace621@gmail.com>
2025-08-11 17:48:11 -0300

    feat(federated): Add placeholder for federated learning utilities

---
+++ src/federated_learning/utils.py
@@ -0,0 +1,7 @@
+# This file is reserved for general utility functions related to the federated learning process
+# that do not fit neatly into the ClientTrainer or FederatedServer classes.
+
+# For example, you could later add functions for custom logging,
+# advanced client selection strategies, etc.
+
+pass

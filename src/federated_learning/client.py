7f2253c3c6d70f8a415fc0838700146afc106931
81wallace18 <patrickwallace621@gmail.com>
2025-12-23 10:45:16 -0300

    feat(fl): implement FedProx for non-IID robustness
    Add FedProxTrainer class that extends HuggingFace Trainer with
    proximal term regularization: L = L_task + (mu/2) * ||w - w_global||^2
    
    This reduces client drift in heterogeneous data scenarios.
    Reference: https://arxiv.org/abs/1812.06127

--- src/federated_learning/client.py
+++ src/federated_learning/client.py
@@ -2,15 +2,54 @@ import os
 import json
 import torch
 from transformers import (
-    AutoTokenizer, 
-    AutoModelForCausalLM, 
+    AutoTokenizer,
+    AutoModelForCausalLM,
     AutoModelForMaskedLM,
-    TrainingArguments, 
-    Trainer, 
+    TrainingArguments,
+    Trainer,
     DataCollatorForLanguageModeling
 )
 from peft import PeftModel
 from datasets import load_from_disk
+
+
+class FedProxTrainer(Trainer):
+    """
+    Custom Trainer that implements FedProx by adding a proximal term to the loss.
+
+    FedProx adds a regularization term that penalizes deviation from the global model:
+    L_total = L_task + (mu/2) * ||w - w_global||^2
+
+    This helps reduce client drift in non-IID scenarios.
+    Reference: https://arxiv.org/abs/1812.06127
+    """
+
+    def __init__(self, global_state_dict=None, fedprox_mu=0.01, **kwargs):
+        super().__init__(**kwargs)
+        self.global_state_dict = global_state_dict
+        self.fedprox_mu = fedprox_mu
+
+    def compute_loss(self, model, inputs, return_outputs=False, **kwargs):
+        """
+        Compute the total loss = task loss + proximal term.
+        """
+        # Compute standard task loss
+        outputs = model(**inputs)
+        task_loss = outputs.loss
+
+        # Add proximal term if FedProx is enabled (mu > 0 and global weights available)
+        if self.fedprox_mu > 0 and self.global_state_dict is not None:
+            proximal_loss = 0.0
+            for name, param in model.named_parameters():
+                if param.requires_grad and name in self.global_state_dict:
+                    global_param = self.global_state_dict[name].to(param.device)
+                    proximal_loss += ((param - global_param) ** 2).sum()
+
+            total_loss = task_loss + (self.fedprox_mu / 2.0) * proximal_loss
+        else:
+            total_loss = task_loss
+
+        return (total_loss, outputs) if return_outputs else total_loss
 
 class ClientTrainer:
     """
@@ -58,9 +97,10 @@ class ClientTrainer:
     def train(self, round_num, learning_rate):
         """
         Executes a single round of training for the client.
+        Supports FedProx regularization when fedprox_mu > 0 in config.
         """
         print(f"--- Starting training for Client {self.client_id} in Round {round_num} ---")
-        
+
         # 1. Load model and dataset
         model = self._load_model_for_training(round_num)
         client_dataset_path = os.path.join(
@@ -72,11 +112,23 @@ class ClientTrainer:
         client_dataset = load_from_disk(client_dataset_path)
         client_dataset = client_dataset.shuffle(seed=round_num)
 
-        # 2. Setup Training Arguments
+        # 2. Capture global model state for FedProx (before training)
+        fedprox_mu = self.config.get('fedprox_mu', 0.0)
+        global_state_dict = None
+        if fedprox_mu > 0:
+            # Clone the global model parameters (only trainable ones for efficiency)
+            global_state_dict = {
+                name: param.data.clone().detach()
+                for name, param in model.named_parameters()
+                if param.requires_grad
+            }
+            print(f"  FedProx enabled with mu={fedprox_mu}")
+
+        # 3. Setup Training Arguments
         training_args = TrainingArguments(
             output_dir=os.path.join(self.config['results_path'], self.config['simulation_name'], "client_training_output"),
             logging_dir=os.path.join(self.config['results_path'], self.config['simulation_name'], "logs"),
-            logging_steps=self.config['max_steps'] + 1, # Avoid logging during training
+            logging_steps=self.config['max_steps'] + 1,  # Avoid logging during training
             learning_rate=learning_rate,
             weight_decay=0.01,
             max_steps=self.config['max_steps'],
@@ -85,30 +137,48 @@ class ClientTrainer:
             per_device_train_batch_size=self.config['batch_size'],
             gradient_accumulation_steps=self.config.get('gradient_accumulation_steps', 1),
             lr_scheduler_type=self.config['lr_scheduler_type'],
-            save_strategy="no", # We save manually
-            #evaluation_strategy="no",
+            save_strategy="no",  # We save manually
         )
 
-        # 3. Setup Trainer
+        # 4. Setup Trainer (FedProxTrainer if mu > 0, else standard Trainer)
         if 'bert' in self.model_name.lower():
             tokenizer = AutoTokenizer.from_pretrained(self.model_name)
             data_collator = DataCollatorForLanguageModeling(
                 tokenizer=tokenizer, mlm=True, mlm_probability=0.15
             )
-            trainer = Trainer(
-                model=model,
-                args=training_args,
-                train_dataset=client_dataset,
-                data_collator=data_collator
-            )
+            if fedprox_mu > 0:
+                trainer = FedProxTrainer(
+                    global_state_dict=global_state_dict,
+                    fedprox_mu=fedprox_mu,
+                    model=model,
+                    args=training_args,
+                    train_dataset=client_dataset,
+                    data_collator=data_collator
+                )
+            else:
+                trainer = Trainer(
+                    model=model,
+                    args=training_args,
+                    train_dataset=client_dataset,
+                    data_collator=data_collator
+                )
         else:
-            trainer = Trainer(
-                model=model,
-                args=training_args,
-                train_dataset=client_dataset,
-            )
-        
-        # 4. Run Training
+            if fedprox_mu > 0:
+                trainer = FedProxTrainer(
+                    global_state_dict=global_state_dict,
+                    fedprox_mu=fedprox_mu,
+                    model=model,
+                    args=training_args,
+                    train_dataset=client_dataset,
+                )
+            else:
+                trainer = Trainer(
+                    model=model,
+                    args=training_args,
+                    train_dataset=client_dataset,
+                )
+
+        # 5. Run Training
         trainer.train()
         print(f"--- Client {self.client_id} training complete. ---")
 
